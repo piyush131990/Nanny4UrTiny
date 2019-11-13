bb26c9d31e3c140bb1fefc27679c24317690eded
piyush131990 <48660637+piyush131990@users.noreply.github.com>
2019-11-13 18:42:16 +1300

    Add files via upload

---
+++ ViewModels/QueryfortempanduserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCManukauTech.ViewModels
+{
+    public class QueryfortempanduserViewModel
+    {
+        [Key]
+        public Int64 id { get; set; }
+        public DateTime Datebook { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
